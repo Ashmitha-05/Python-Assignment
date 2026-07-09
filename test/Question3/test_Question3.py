7d283b6c5bcd42837573b80bbc08f06e32bcbb39
Ash <ashmitha.v@diggibyte.com>
2026-07-09 16:20:33 +0530

    test_question3

---
+++ test/Question3/test_Question3.py
@@ -0,0 +1,15 @@
+import util
+
+def test_find_second_large():
+    nums = [2, 3, 6, 6, 5]
+    assert util.find_second_large(nums) == 5
+
+
+def test_find_second_large_negative():
+    nums = [-2, -1, -5]
+    assert util.find_second_large(nums) == -2
+
+
+def test_find_second_large_duplicates():
+    nums = [10, 10, 8, 7]
+    assert util.find_second_large(nums) == 8
