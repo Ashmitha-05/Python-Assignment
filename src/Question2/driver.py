edf110833820f5d91aab4d2c576634d1efa19c35
Ash <ashmitha.v@diggibyte.com>
2026-07-09 12:33:09 +0530

    finding average q2

--- src/Question2/driver.py
+++ src/Question2/driver.py
@@ -1,1 +1,19 @@
+from util import *
 
+n = int(input())
+
+student_dict = {}
+
+for i in range(n):
+    student_details = input().split(" ")
+    student_name = student_details[0]
+    student_mark = list(map(float, student_details[1:]))
+
+    add_student(student_dict, student_name, student_mark)
+
+name = input()
+
+average = calculate_average(student_dict, name)
+
+if average is not None:
+    print(f"{average:.2f}")
--- src/Question2/util.py
+++ src/Question2/util.py
@@ -1,1 +1,16 @@
+def add_student(student_dict, student_name, student_mark):
+    student_dict[student_name] = student_mark
 
+
+def calculate_average(student_dict, name):
+    total = 0
+    count = 0
+
+    if name in student_dict:
+        for marks in student_dict[name]:
+            count += 1
+            total += marks
+
+        return total / count
+
+    return None
