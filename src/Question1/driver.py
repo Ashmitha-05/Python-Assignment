5f5a4271357c249df85b400facff1cd385c2c31a
Ash <ashmitha.v@diggibyte.com>
2026-07-09 11:55:02 +0530

    Lists from q1

--- src/Question1/driver.py
+++ src/Question1/driver.py
@@ -1,1 +1,27 @@
+from util import *
+n = int(input())
+lst = []
 
+for i in range(n):
+    command = input().split()
+
+    if command[0] == "insert":
+        insert_no(lst, int(command[1]), int(command[2]))
+
+    elif command[0] == "append":
+        append_no(lst, int(command[1]))
+
+    elif command[0] == "remove":
+        remove_no(lst, int(command[1]))
+
+    elif command[0] == "sort":
+        sort_list(lst)
+
+    elif command[0] == "pop":
+        pop_no(lst)
+
+    elif command[0] == "reverse":
+        reverse_list(lst)
+
+    elif command[0] == "print":
+        print_list(lst)
--- src/Question1/util.py
+++ src/Question1/util.py
@@ -1,1 +1,21 @@
 
+def insert_no(lst, index, element):
+    lst.insert(index, element)
+
+def append_no(lst, element):
+    lst.append(element)
+
+def remove_no(lst, element):
+    lst.remove(element)
+
+def sort_list(lst):
+    lst.sort()
+
+def pop_no(lst):
+    lst.pop()
+
+def reverse_list(lst):
+    lst.reverse()
+
+def print_list(lst):
+    print(lst)
