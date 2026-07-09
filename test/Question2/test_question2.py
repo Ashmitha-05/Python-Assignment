e69299ee913b6c390de694dcb1ecda3f0ac3e00a
Ash <ashmitha.v@diggibyte.com>
2026-07-09 16:19:01 +0530

    test_question2

--- test/Question2/test_question2.py
+++ test/Question2/test_question2.py
@@ -1,1 +1,17 @@
+import util
 
+def test_calculate_average():
+    student_dict = {
+        "Krishna": [67.0, 68.0, 69.0],
+        "Malika": [52.0, 56.0, 60.0]
+    }
+
+    assert util.calculate_average(student_dict, "Malika") == 56.0
+
+
+def test_calculate_average_single_student():
+    student_dict = {
+        "Ash": [90.0, 80.0, 70.0]
+    }
+
+    assert util.calculate_average(student_dict, "Ash") == 80.0
