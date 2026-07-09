1e5d7e878ef0680fb7b623ec5583d95cd5204f87
Ash <ashmitha.v@diggibyte.com>
2026-07-09 16:11:20 +0530

    test_q1

---
+++ test/Question1/test_Question1.py
@@ -0,0 +1,36 @@
+import util
+
+def test_insert_element():
+    lst = [1, 2, 3]
+    util.insert_no(lst, 1, 5)
+    assert lst == [1, 5, 2, 3]
+
+
+def test_append_element():
+    lst = [1, 2]
+    util.append_no(lst, 3)
+    assert lst == [1, 2, 3]
+
+
+def test_remove_element():
+    lst = [1, 2, 3]
+    util.remove_no(lst, 2)
+    assert lst == [1, 3]
+
+
+def test_sort_list():
+    lst = [3, 1, 2]
+    util.sort_list(lst)
+    assert lst == [1, 2, 3]
+
+
+def test_pop_element():
+    lst = [1, 2, 3]
+    util.pop_element(lst)
+    assert lst == [1, 2]
+
+
+def test_reverse_list():
+    lst = [1, 2, 3]
+    util.reverse_list(lst)
+    assert lst == [3, 2, 1]
